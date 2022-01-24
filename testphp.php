52b4dd9f028473d31ac62a7e107154c93fe007a5
Christian Pezzin <christian.pezzin@gmail.com>
2022-01-24 18:29:34 +0100

    Add files via upload
    Added a test query

--- testphp.php
+++ testphp.php
@@ -8,7 +8,18 @@ $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 
-echo $conn;
+$sql = "SELECT * from rooms";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Unique ID: " . $row["unique_id"]. " - Status: " . $row["status"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+$conn->close();
 
 ?>
